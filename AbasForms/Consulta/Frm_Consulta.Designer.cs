9072e55af5ef6a714df10aab761ea64e5f930eeb
Yannis Bianchini Pontuschka <56230345+Yax1234566@users.noreply.github.com>
2023-12-17 14:51:32 -0300

    Adição da funcionalidade de gerenciamento de serviços. Opções de busca de serviços:   1.  na semana;   2.  no mês   3.  por funcionário ou   4.  por cliente. Adicionei também uma busca automática que apresenta todas as consultas do dia.

---
+++ AbasForms/Consulta/Frm_Consulta.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ClinicaVeterinariaBD
+{
+    partial class Frm_Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SchedulingViewer = new DataGridView();
+            btn_BuscaServico = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)SchedulingViewer).BeginInit();
+            SuspendLayout();
+            // 
+            // SchedulingViewer
+            // 
+            SchedulingViewer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            SchedulingViewer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            SchedulingViewer.Location = new Point(3, 34);
+            SchedulingViewer.Name = "SchedulingViewer";
+            SchedulingViewer.RowTemplate.Height = 25;
+            SchedulingViewer.Size = new Size(685, 178);
+            SchedulingViewer.TabIndex = 0;
+            // 
+            // btn_BuscaServico
+            // 
+            btn_BuscaServico.Location = new Point(157, 252);
+            btn_BuscaServico.Name = "btn_BuscaServico";
+            btn_BuscaServico.Size = new Size(196, 45);
+            btn_BuscaServico.TabIndex = 1;
+            btn_BuscaServico.Text = "Buscar por serviço";
+            btn_BuscaServico.UseVisualStyleBackColor = true;
+            btn_BuscaServico.Click += btn_BuscaServico_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Consultas do dia";
+            // 
+            // Frm_Consulta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(label1);
+            Controls.Add(btn_BuscaServico);
+            Controls.Add(SchedulingViewer);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Frm_Consulta";
+            Text = "Frm_Consulta";
+            ((System.ComponentModel.ISupportInitialize)SchedulingViewer).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView SchedulingViewer;
+        private Button btn_BuscaServico;
+        private Label label1;
+    }
+}
