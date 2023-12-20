8a804434721e046272f75f90ffbb5c8c035e0b6d
IrPedro <irpedrogsp@usp.br>
2023-12-20 12:56:42 -0300

    Revert "Criada Pasta Cliente_Pet com Forms dos Clientes e Forms dos Animais do Cliente escolhido (Não se esqueçam de mudar a senha)"
    This reverts commit 80000b2c0a272de0ebe887d50d2278295f40023a.

---
+++ AbasForms/Frm_ClientePET.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace ClinicaVeterinariaBD.AbasForms
+{
+    partial class Frm_ClientePET
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Frm_ClientePET
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Frm_ClientePET";
+            Text = "Frm_ClientePET";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
